6e3fa6a46d6076515f2cdbebcb35c89dd0b8d3ee
Tim Mussack <tim.mussack@gmail.com>
2022-12-31 15:08:49 -0600

    Updated express server index file

--- server/index.js
+++ server/index.js
@@ -1,45 +1,29 @@
 const express = require('express');
-const path = require('path');
-const axios = require('axios');
+//const path = require('path');
 const compression = require('compression');
 require('dotenv').config();
 const { getReviews, getMeta, putHelpful, putReport, postReview } = require('../db/controllers.js');
-
 const app = express();
+
 app.use(compression({level: 9}));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../Client/dist')));
+//app.use(express.urlencoded({ extended: true }));
+//app.use(express.static(path.join(__dirname, '../Client/dist')));
 app.use(express.json());
 
 //Get reviews, Status: 200 OK
-app.get('/reviews', (req, res) => {
-  const { product_id, count, sort, page } = req.query;
-
-});
+app.get('/reviews', getReviews);
 
 //Get review meta, Status: 200 OK
-app.get('/reviews/meta', (req, res) => {
-  const { product_id } = req.query;
-
-});
+app.get('/reviews/meta', getMeta);
 
 //Add review to database, Status: 201 CREATED
-app.post('/reviews', (req, res) => {
-  const { product_id, body, rating, recommend, name, summary, email, photos, characteristics } = req.body;
-
-});
+app.post('/reviews', postReview);
 
 //Mark review as helpful, Status: 204 NO CONTENT
-app.put('/reviews/:review_id/helpful', (req, res) => {
-  const { review_id } = req.body;
-
-});
+app.put('/reviews/:review_id/helpful', putHelpful);
 
 //Report a review, Status: 204 NO CONTENT
-app.put('/reviews/:review_id/report', (req, res) => {
-  const { review_id } = req.body;
-
-});
+app.put('/reviews/:review_id/report', putReport);
 
 const port = process.env.PORT;
 app.listen(port, () => {
